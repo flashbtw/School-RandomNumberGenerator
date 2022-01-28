e976e18708447526d439a3bc8894e2522da0d1e8
flashbtw <johannes-kurt.reiter@freenet.de>
2022-01-28 21:02:39 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,83 @@
+function n_zz2(lowerBound, upperBound, count) {
+    let unsortedIntegerArray = [];
+    let sortedIntegerArray = [];
+    let randomNumber;
+    let i = 0;
+
+    console.log(lowerBound, upperBound, count);
+    if (!inputValid(lowerBound, upperBound, count)) {
+        return;
+    }
+
+    for (i = 0; i < count; i++) {
+        randomNumber = randomNumberGenerator(lowerBound, upperBound)
+        if (isNumberUniqueInArray(unsortedIntegerArray, randomNumber)) {
+            unsortedIntegerArray[i] = randomNumber;
+        } else {
+            i--           
+        }
+    }
+
+    sortedIntegerArray = sortIntegerArrayWithBubblesort(unsortedIntegerArray);
+    document.getElementById("OutputRandomNumbers").innerHTML = sortedIntegerArray.join(", ").toString();
+}
+
+function inputValid(lowerBound, upperBound, count) {
+    if (lowerBound >= upperBound) {
+        document.getElementById("Error_td").style = "background-color: lightcoral;";
+        document.getElementById("Errors").innerHTML = "untere Grenze kann nicht groeßer als obere sein."
+        return false;
+    }
+    if (upperBound - lowerBound == count) {
+        document.getElementById("Error_td").style = "background-color: lightcoral;";
+        document.getElementById("Errors").innerHTML = "kann nicht mehr einzigartige Zahlen erstellen, als es gibt"
+        return false;
+    }
+
+    return true;
+}
+
+function randomNumberGenerator(lowerBound, upperBound) {
+    let random = Math.round(Math.random() * (upperBound - 1))
+    return random + 1;
+}
+
+function isNumberUniqueInArray(integerArray, toBeCheckedNumber) {
+    let arrayLength = integerArray.length;
+    let numberUnique = true;
+    
+    if (arrayLength > 0) {
+        for (i = 0; i < arrayLength && numberUnique; i++) {
+            if (integerArray[i] == toBeCheckedNumber) {
+                numberUnique = false;
+            }
+        }
+    }
+    return numberUnique;
+}
+
+function sortIntegerArrayWithBubblesort(unsortedIntegerArray) {
+    let i;
+    let memory;
+    let sorted = false;
+    let sortedIntegerArray;
+
+    while (!sorted) {
+        sorted = true;
+        for (i = 0; i < unsortedIntegerArray.length; i++) {
+            if (unsortedIntegerArray[i] > unsortedIntegerArray[i + 1]) {
+                sorted = false;
+                memory = unsortedIntegerArray[i];
+                unsortedIntegerArray[i] = unsortedIntegerArray[i + 1];
+                unsortedIntegerArray[i + 1] = memory;
+            }
+        }
+    }
+    sortedIntegerArray = unsortedIntegerArray;
+    return sortedIntegerArray;
+}
+
+function resetErrors() {
+    document.getElementById("Errors").innerHTML = "";
+    document.getElementById("Error_td").style = "background-color: aquamarine;";
+}
